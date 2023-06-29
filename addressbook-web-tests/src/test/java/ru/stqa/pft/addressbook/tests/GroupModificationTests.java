d3b77fd5576487fa080e03641ab1f9123f155e39
Roman Lamzin <lamzinrn@gmail.com>
2023-06-29 20:29:36 +0400

    add getGroupList
    Добавлен метод getGroupList в класс создания, удаления, модификацию Групп.
    Исправлено сравнение, добавлен size()

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,27 +4,27 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
 
-    int before = app.getGroupHelper().getGroupCount();
-
-    if (! app.getGroupHelper().isThereGroup()) {
+    if (!app.getGroupHelper().isThereGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test", null, null));
     }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
 
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test_Modification", "test_header_Modification", "test_footer_Modification"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
 
-    int after = app.getGroupHelper().getGroupCount();
-
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
