532447a31246fc0d9e4042fd942e78f16f9f29ea
Roman Lamzin <lamzinrn@gmail.com>
2023-07-01 19:34:01 +0400

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -15,7 +15,6 @@ public class GroupHelper extends HelperBase {
   }
 
 
-
   public void returnToGroupPage() {
     click(By.linkText("groups"));
   }
@@ -72,7 +71,8 @@ public class GroupHelper extends HelperBase {
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
-      GroupData group = new GroupData(name, null,null);
+      String id = element.findElement(By.tagName("input")).getAttribute("value");
+      GroupData group = new GroupData(id, name, null, null);
       groups.add(group);
     }
     System.out.println(" Check array __ " + groups);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,12 +20,21 @@ public class GroupModificationTests extends TestBase {
 
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test_Modification", "test_header_Modification", "test_footer_Modification"));
+
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test_Modification", "test_header_Modification", "test_footer_Modification");
+
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
 
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+
+  before.remove(before.size() - 1);
+  before.add(group);
+  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 
 }
