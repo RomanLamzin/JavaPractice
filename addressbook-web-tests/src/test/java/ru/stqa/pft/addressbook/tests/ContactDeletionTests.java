697b76c69ab0b91955fd2d3ad0c784fd9c73b790
Roman Lamzin <lamzinrn@gmail.com>
2023-07-22 15:45:30 +0400

    Лекция 4.6. Сравнение коллекций
    Добавил сравнение списков целиком в класс ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
@@ -18,7 +17,8 @@ public class ContactDeletionTests extends TestBase {
     if (!app.getContactHelper().isThereContact()) {
       app.getContactHelper().createContact(new ContactData("Roman", "Lamzin", "New-Studio",
               "Samara", "89277778781", "lamzinrn@gmail.com", "test"));
-    };
+    }
+    ;
     List<ContactData> before = app.getContactHelper().getContactList(); // v2
     //   int before = app.getContactHelper().getContactCount(); // использовать после проверки предусловия // v1
 
@@ -32,6 +32,12 @@ public class ContactDeletionTests extends TestBase {
     // Assert.assertEquals(after, before - 1); // v1
 
     Assert.assertEquals(after.size(), before.size() - 1); // при работе со списками используем метод size // v2
+
+
+    before.remove(before.size() - 1); // удаляем из списка один элемент
+
+    Assert.assertEquals(before, after);
+
 
   }
 
